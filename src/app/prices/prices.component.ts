a242ca0d7c92f44c78f8f1ca022dca1f618092ad
Chandler  Wolters <chandlerwolters@Chandlers-Air.attlocal.net>
2024-01-18 22:32:51 -0600

    fix: small

--- src/app/prices/prices.component.ts
+++ src/app/prices/prices.component.ts
@@ -5,23 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './prices.component.html',
   styleUrls: ['./prices.component.scss']
 })
-export class PricesComponent implements OnInit{
+export class PricesComponent implements OnInit {
 
   total = 0;
   guest = 100;
   bar = 'Standard';
   day = 'Weekend';
   all_selected_values: number[] = [];
+  limitedBarAmount = 0;
 
   ngOnInit(): void {
     this.getPrice();
   }
   getPrice() {
     let addOns = 0;
-    for (let i = 0; i < this.all_selected_values.length; i++ ) {
+    for (let i = 0; i < this.all_selected_values.length; i++) {
       addOns += this.all_selected_values[i];
     }
-    this.total = ((this.day === 'Weekend') ? 3000 : 1500) + this.guest * (((this.bar === 'Standard') ? 16 : 19) + 16) + 250 + addOns;
+    let barPrice;
+    if (this.bar === 'Standard') {
+      barPrice = 16
+    } else if (this.bar === 'Premium') {
+      barPrice = 19
+    } else if (this.bar === 'Limited Bar') {
+      barPrice = this.limitedBarAmount;
+      this.total = ((this.day === 'Weekend') ? 3000 : 1500) + this.guest * 16 + (barPrice) + 250 + addOns;
+      return;
+    } else{
+      barPrice = 0;
+    }
+    this.total = ((this.day === 'Weekend') ? 3000 : 1500) + this.guest * (barPrice +  16) + 250 + addOns;
   }
   onChange(value: number): void {
     if (this.all_selected_values.includes(value)) {
